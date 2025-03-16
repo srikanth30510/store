95579e16f95fedb355eb7d21ade80e6b192cb706
Srikanth Godavari <110710256+srikanth30510@users.noreply.github.com>
2025-03-16 18:35:01 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
   theme: {
     extend: {
       borderRadius: {
+        
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
