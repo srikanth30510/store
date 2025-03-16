5e7923c32156e97199b1a518a4f85e6c684a883c
Srikanth Godavari <110710256+srikanth30510@users.noreply.github.com>
2025-03-16 18:33:10 +0530

    Updated drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
+
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
