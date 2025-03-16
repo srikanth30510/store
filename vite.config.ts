25e597bcbe1bf9677bbe2d291070f4e958ec83cc
Srikanth Godavari <110710256+srikanth30510@users.noreply.github.com>
2025-03-16 18:35:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
