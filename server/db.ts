97fb9278fd2c63c2eaf4e07b46b091e87e660f49
Srikanth Godavari <110710256+srikanth30510@users.noreply.github.com>
2025-03-16 18:25:57 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes("localhost") ? false : { rejectUnauthorized: false }, // Disable SSL for local
+  ssl: process.env.DATABASE_URL.includes("localhost") ? false : { rejectUnauthorized: false }, 
 });
 
 export const db = drizzle(pool, { schema });
